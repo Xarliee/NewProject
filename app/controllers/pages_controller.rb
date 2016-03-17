fe91b3afdbbf790cc700ef450ecdfc688370fa15
Carlos <c.salas.marin@gmail.com>
2016-03-16 21:03:53 -0300

    ejercicio 25

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,24 @@
 class PagesController < ApplicationController
+ before_action :get_data
+
   def pag1
+  render layout:"landing"
+  puts "#{@users}"
   end
 
   def pag2
+  puts "#{@users}"
   end
+  
+  def pag3
+  puts "#{@users}"
+  end
+  
+  private 
+
+  def get_data 
+  	 @users = ["Carlos","Jose","Pedro"] 
+  	 puts "#{@users}"
+  end
+
 end
